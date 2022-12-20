06d3869a89410bac21df0cda2c381d038fef10c3
TetyanaKhomenko <87753182+TetyanaKhomenko@users.noreply.github.com>
2022-12-20 14:38:10 +0200

    Add files via upload

---
+++ ДЗ4/data.py
@@ -0,0 +1,24 @@
+import re
+import spacy
+import pymorphy2
+
+with open(r'C:\Users\Note\Desktop\панченко\4\text_for_db.txt', 'r', encoding='utf-8') as f:
+    text = f.read()
+
+nlp = spacy.load("uk_core_news_sm")
+doc = nlp(text)
+normal_forms = {}
+for token in doc:
+    if re.match(r"[А-ЩЬЮЯҐЄІЇа-щьюяґєії'`’ʼ]", token.text):
+        normal_forms[token.lemma_]=token.pos_
+
+
+
+morph = pymorphy2.MorphAnalyzer(lang='uk')
+for k,v in normal_forms.items():
+    normal_forms[k] = (v, [inf[0] for inf in morph.parse (k)[0].lexeme])
+
+print(normal_forms)
+# print(words)
+# for k,v in words.items():
+#     print(k,'—',v)
